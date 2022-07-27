d8f65682f28586efd8e70c98201fa9222c85d351
I2rys <i2rys@protonmail.com>
2022-07-27 09:07:18 +0800

    Refactored

--- README.md
+++ README.md
@@ -1,30 +1,24 @@
-<h1 align="center">DorkyDump</h1>
-<h4 align="center">Dork links results dumper</h4>
-<p align="center">
-	<a href="https://github.com/I2rys/DorkyDump/blob/main/LICENSE"><img src="https://img.shields.io/github/license/I2rys/DorkyDump?style=flat-square"></img></a>
-	<a href="https://github.com/I2rys/DorkyDump"><img src="https://bettercodehub.com/edge/badge/I2rys/DorkyDump?branch=main"></a>
-	<a href="https://github.com/I2rys/DorkyDump/issues"><img src="https://img.shields.io/github/issues/I2rys/DorkyDump.svg"></img></a>
-	<a href="https://github.com/I2rys/DorkyDump"><img src="https://img.shields.io/badge/version-1.0.0-orange"></img></a>
-	<a href="https://nodejs.org/"><img src="https://img.shields.io/badge/-Nodejs-green?style=flat-square&logo=Node.js"></img></a>
-</p>
-
-
+# DorkyDump
+Dork links results dumper.
 ## Installation
 Github:
+```
+git clone https://github.com/I2rys/dorkydump
+```
 
-    git clone https://github.com/I2rys/DorkyDump
+NpmJS:
+```
+npm i puppeteer chalk fs
+```
 
-NPM Packages:
-
-    npm i puppeteer && npm i chalk && npm i fs
-    
 ## Usage
+```
+node index.js <maxPage> <output> <dork>
+```
 
-    node index.js <max_page> <output> <dork>
-
- - max_page - The maximum pages to get the links if possible.
- - output - The output of the results.
- - dork - The dork to use.
+- maxPage - The maximum pages to get links.
+- output - The output file of the results.
+- dork - The dork to use.
 
 ## License
 MIT © I2rys
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
+"use strict";
+
 //Dependencies
-const Puppeteer = require("puppeteer")
-const Chalk = require("chalk")
-const Fs = require("fs")
+const puppeteer = require("puppeteer")
+const chalk = require("chalk")
+const fs = require("fs")
 
 //Variables
-const Self_Args = process.argv.slice(2)
+const args = process.argv.slice(2)
 
-var DorkyDump_Data = {}
-DorkyDump_Data.links = []
+var DorkyDump = {
+    links: []
+}
 
 //Functions
-async function Get_Links(){
-    const browser = await Puppeteer.launch({ headless: false, argvs: ["--no-sandbox", "--disable-setuid-sandbox"] })
+async function gatherLinks(){
+    const browser = await puppeteer.launch({ headless: false, argvs: ["--no-sandbox", "--disable-setuid-sandbox"] })
     const page = await browser.newPage()
 
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36")
-    await page.goto(`https://www.bing.com/search?q=${Self_Args.slice(2).join("")}`)
+    await page.goto(`https://www.bing.com/search?q=${args.slice(2).join("")}`)
 
-    const page_content = await page.content()
+    const pageContent = await page.content()
 
-    if(page_content.indexOf("There are no results for") != -1){
-        console.log(`${Chalk.grey("[") + Chalk.yellowBright("WARNING") + Chalk.grey("]")} Something went wrong while gathering some links, please try again later.`)
-        console.log(`${Chalk.grey("[") + Chalk.blueBright("INFO") + Chalk.grey("]")} Aborting..`)
+    if(pageContent.indexOf("There are no results for") !== -1){
+        console.log(`${chalk.grey("[") + chalk.yellowBright("WARNING") + chalk.grey("]")} Something went wrong while gathering some links, please try again later.`)
+        console.log(`${chalk.grey("[") + chalk.blueBright("INFO") + chalk.grey("]")} Aborting..`)
         browser.close()
         process.exit()
-        return
     }
 
-    var page_index = 1
+    var pageIndex = 1
 
     await page.waitForSelector("#b_results > li> h2 > a").catch(()=>{
-        console.log(`${Chalk.grey("[") + Chalk.yellowBright("WARNING") + Chalk.grey("]")} Something went wrong while gathering some links, please try again later.`)
-        console.log(`${Chalk.grey("[") + Chalk.blueBright("INFO") + Chalk.grey("]")} Aborting..`)
+        console.log(`${chalk.grey("[") + chalk.yellowBright("WARNING") + chalk.grey("]")} Something went wrong while gathering some links, please try again later.`)
+        console.log(`${chalk.grey("[") + chalk.blueBright("INFO") + chalk.grey("]")} Aborting..`)
         browser.close()
         process.exit()
-        return
     })
 
     const links = await page.$$eval("#b_results > li> h2 > a", elems =>{
         return elems.map(elem => elem.getAttribute("href"))
     })
 
-    for( i in links ){
-        DorkyDump_Data.links.push(links[i])
+    for( let i in links ){
+        DorkyDump.links.push(links[i])
     }
 
-    page_index += 1
+    pageIndex++
 
-    Repeater()
-    async function Repeater(){
-        await page.click(`#b_results > li.b_pag > nav > ul > li:nth-of-type(${page_index}) > a`).catch(()=>{
-            console.log(`${Chalk.grey("[") + Chalk.yellowBright("WARNING") + Chalk.grey("]")} Max page detected, aborting links gatherer.`)
-            console.log(`${Chalk.grey("[") + Chalk.blueBright("INFO") + Chalk.grey("]")} ${DorkyDump_Data.links.length} links has been gathered. Continuing to phase 2.`)
+    gather()
+    async function gather(){
+        await page.click(`#b_results > li.b_pag > nav > ul > li:nth-of-type(${pageIndex}) > a`).catch(()=>{
+            console.log(`${chalk.grey("[") + chalk.yellowBright("WARNING") + chalk.grey("]")} Max page detected, aborting links gatherer.`)
+            console.log(`${chalk.grey("[") + chalk.blueBright("INFO") + chalk.grey("]")} ${DorkyDump.links.length} links has been gathered.`)
             browser.close()
-            D()
-            return
+            return done()
         })
         await page.waitForSelector("#b_results > li> h2 > a")
 
@@ -62,65 +62,32 @@ async function Get_Links(){
             return elems.map(elem => elem.getAttribute("href"))
         })
     
-        for( i in links ){
-            DorkyDump_Data.links.push(links[i])
-        }
+        for( const link of links ) DorkyDump.links.push(link)
 
-        if(page_index == Self_Args[0]){
-            console.log(`${Chalk.grey("[") + Chalk.blueBright("INFO") + Chalk.grey("]")} ${DorkyDump_Data.links.length} links has been gathered. Continuing to phase 2.`)
-            await browser.close()
-            D()
-            return
+        if(pageIndex == args[0]){
+            console.log(`${chalk.grey("[") + chalk.blueBright("INFO") + chalk.grey("]")} ${DorkyDump.links.length} links has been gathered.`)
+            browser.close()
+            return end()
         }
     
-        page_index += 1
+        pageIndex++
 
-        Repeater()
+        gather()
         return
     }
 }
 
-function D(){
-    var links = ""
-
-    for( i in DorkyDump_Data.links ){
-        if(links.lengh == 0){
-            links = DorkyDump_Data.links[i]
-        }else{
-            links += `\n${DorkyDump_Data.links[i]}`
-        }
-    }
-
-    Fs.writeFileSync(Self_Args[1], links, "utf8")
-    console.log("Done!")
+function end(){
+    fs.writeFileSync(args[1], DorkyDump.links.join("\n"), "utf8")
+    console.log("Finished.")
 }
 
 //Main
-if(Self_Args.length == 0){
-    console.log(`node index.js <max_page> <output> <dork>
-Example: node index.js 2 ./test_output.txt Dork: "Index of" "upload_image.php"`)
-    process.exit()
-}
+if(!args.length) return console.log("node index.js <maxPage> <output> <dork>")
+if(!args[0]) return console.log("Invalid max page.")
+if(isNaN(args[0])) return console.log("Max page is not a number.")
+if(!args[1]) return console.log("Invalid output.")
+if(!args[2]) return console.log("Invalid dork.")
 
-if(Self_Args[0] == ""){
-    console.log(`${Chalk.grey("[") + Chalk.redBright("ERROR") + Chalk.grey("]")} Invalid max_page.`)
-    process.exit()
-}
-
-if(isNaN(Self_Args[0])){
-    console.log(`${Chalk.grey("[") + Chalk.redBright("ERROR") + Chalk.grey("]")} max_page is not an Int.`)
-    process.exit()
-}
-
-if(Self_Args[1] == ""){
-    console.log(`${Chalk.grey("[") + Chalk.redBright("ERROR") + Chalk.grey("]")} Invalid output.`)
-    process.exit()
-}
-
-if(Self_Args[2] == ""){
-    console.log(`${Chalk.grey("[") + Chalk.redBright("ERROR") + Chalk.grey("]")} Invalid dork.`)
-    process.exit()
-}
-
-console.log(`${Chalk.grey("[") + Chalk.blueBright("INFO") + Chalk.grey("]")} Dorking has started.`)
-Get_Links()
+console.log(`${chalk.grey("[") + chalk.blueBright("INFO") + chalk.grey("]")} Dorking has started.`)
+gatherLinks()
